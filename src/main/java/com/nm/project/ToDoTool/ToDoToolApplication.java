02dc52d48edcb308e2fe03c376375915598fc4b6
niran <niranjanamohan18.00@gmail.com>
2025-03-09 17:15:11 +0530

    Initial commit

---
+++ src/main/java/com/nm/project/ToDoTool/ToDoToolApplication.java
@@ -0,0 +1,13 @@
+package com.nm.project.ToDoTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nm/project/ToDoTool/ToDoToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nm.project.ToDoTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
